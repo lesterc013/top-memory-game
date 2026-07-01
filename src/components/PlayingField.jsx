a6a3727121ac3b10b1a6e3c2f5de98a8fb9d29fa
lester-mtse-machine <lesterc013@gmail.com>
2026-07-01 15:33:10 +0800

    new-component: Synchronises required cardData state on first render
    Refer to the useEffect comments to understand the flow.

---
+++ src/components/PlayingField.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import Card from './Card';
+
+const API_ENDPOINT = 'https://rickandmortyapi.com/api/character';
+const NUM_CARDS = 12;
+
+export default function PlayingField() {
+  const [cardData, setCardData] = useState([]);
+
+  // Initialise the 12 cardData objects.
+  useEffect(() => {
+    const cardDataPlaceholder = [];
+    const promises = [];
+    const controller = new AbortController();
+
+    async function getCardData(charId) {
+      try {
+        let res = await fetch(`${API_ENDPOINT}/${charId}`, {
+          signal: controller.signal,
+        });
+        res = await res.json();
+        cardDataPlaceholder.push(res);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    // Run all the fetches
+    for (let i = 0; i < NUM_CARDS; i++) {
+      const promise = getCardData(i + 1);
+      promises.push(promise);
+    }
+
+    // Only set state when all fetch results resolve.
+    Promise.all(promises).then(() => setCardData(cardDataPlaceholder));
+
+    // Clean up function using master switch to abort all fetch requests at once.
+    return () => {
+      if (controller) {
+        controller.abort();
+        console.log('All fetch requests aborted');
+      }
+    };
+  }, []);
+
+  return (
+    <div className='playing-field'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
