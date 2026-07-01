967935009b624c9924b13cdf9ea4db947afeaf94
lester-mtse-machine <lesterc013@gmail.com>
2026-07-01 11:02:57 +0800

    Make basic structural styling of the big pieces in the App

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,30 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>Memory Game</h1>
-    </>
+    <div className='app-container'>
+      <div className='header'>
+        <div className='title'>Title</div>
+        <div className='score-container'>
+          <div className='best-score'>Best Score</div>
+          <div className='current-score'>Current Score</div>
+        </div>
+      </div>
+
+      <div className='playing-field'>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+        <div className='card'>Card</div>
+      </div>
+    </div>
   );
 }
 
