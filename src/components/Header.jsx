b1a2353cf1c680b80d5b65ee253bf6eacfe39d37
lester-mtse-machine <lesterc013@gmail.com>
2026-07-02 11:28:12 +0800

    Update words to render

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import '../styles/Header.css';
 export default function Header({ bestScore, currentScore }) {
   return (
     <div className='header'>
-      <div className='title'>Rick n Morty's Memory Game WUBBALUBBADUBDUB</div>
+      <div className='title'>Rick n Morty Memory Game WUBBALUBBADUBDUB</div>
       <div className='score-container'>
-        <div className='best-score'>Best Score: {bestScore}</div>
-        <div className='current-score'>Current Score: {currentScore}</div>
+        <div className='best-score'>Best: {bestScore}</div>
+        <div className='current-score'>Current: {currentScore}</div>
       </div>
     </div>
   );
